00e908c212c0cebbe756864cdc89e51d1a5d2c7a
Tobic7 <tobicaminos5@gmail.com>
2024-07-23 18:00:20 -0300

    funcion para que se envie el formulario con php

---
+++ procesar_formulario.php
@@ -0,0 +1,33 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre_empresa = htmlspecialchars(trim($_POST['nombre_empresa']));
+    $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+    $telefono = htmlspecialchars(trim($_POST['telefono']));
+    $mensaje = htmlspecialchars(trim($_POST['mensaje']));
+
+    if (!empty($nombre_empresa) && !empty($email) && !empty($telefono) && !empty($mensaje)) {
+
+        // Configuración del correo //
+        $to = "tobicaminos5@gmail.com"; // Cambiar al correo de la empresa
+        $subject = "Nuevo mensaje de la Web";
+        $body = "Nombre de la Empresa: $nombre_empresa\n";
+        $body .= "Email: $email\n";
+        $body .= "Teléfono: $telefono\n";
+        $body .= "Mensaje:\n$mensaje";
+
+        $headers = "From: $email";
+
+        // Enviar el correo
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Mensaje enviado exitosamente.";
+
+        } else {
+            echo "Error al enviar el mensaje. Inténtalo de nuevo más tarde.";
+        }
+
+    } else {
+        echo "Por favor, complete todos los campos.";
+    }
+}
+?>
